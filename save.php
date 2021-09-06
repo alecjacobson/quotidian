d7776f680d0840fe8af6fa0e61099b1c5b1d3e90
Alec Jacobson <alecjacobson@gmail.com>
2021-09-06 16:20:13 -0400

    working ledger

--- save.php
+++ save.php
@@ -1,7 +1,8 @@
 <?php
   $data = $_POST['data'];
-  $file = fopen('counts.json','w');
+  $file = fopen('ledger.json','w');
   fwrite($file, $data);
   fclose($file);
-  echo strlen($data);
+  $response = shell_exec('git -c user.name="www-data" -c user.email="no-replay@example.org" commit -m "from save.php" ledger.json');
+  echo strlen($response);
 ?>
